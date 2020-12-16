b6a5ca119b27306b5bfe4d148b82f66aa741c9d0
Stormpero <staroselsky.S@yandex.ru>
2020-12-17 01:26:30 +0300

    nn

--- structs2020/mainwindow.h
+++ structs2020/mainwindow.h
@@ -18,80 +18,191 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+/*!
+ \brief
+
+*/
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    /*!
+     \brief Конструктор
+
+     \param parent
+    */
     MainWindow(QWidget *parent = nullptr);
+    /*!
+     \brief
+
+    */
     ~MainWindow();
 
-//show window and design functions
+
 private:
-    Ui::MainWindow *ui;
+    Ui::MainWindow *ui; /*!< TODO: describe */
+    /*!
+     \brief
+
+    */
     void initializationCSS();
+    /*!
+     \brief
+
+    */
     void IntitialiseApp();
+    /*!
+     \brief
+
+    */
     void CreateToolBars();
+    /*!
+     \brief
+
+    */
     void InitialiseConnections();
+    /*!
+     \brief
+
+     \param a
+    */
     void disableMenu(bool a);
+    /*!
+     \brief
+
+    */
     void CreateStatusBar();
+    /*!
+     \brief
+
+    */
     void updateFileSize();
 
-//Toolbar functions
+
 private:
+    /*!
+     \brief
+
+     \return QToolBar
+    */
     QToolBar* createMainToolBar();
+    /*!
+     \brief
+
+     \return QToolBar
+    */
     QToolBar* createExtraToolBar();
+    /*!
+     \brief
+
+     \return QToolBar
+    */
     QToolBar* createWorkToolBar();
 
-//Toolbar variable to set it enabled or disabled when needed
-private:
-    QToolBar *WorkToolBar;
-    Document m_content;
 
-//Toolbar buttons
+private:
+    QToolBar *WorkToolBar; /*!< TODO: describe */
+    Document m_content; /*!< TODO: describe */
+
+
 private slots:
+    /*!
+     * \brief MarkdowneditCheck
+     */
     void MarkdowneditCheck();
     void PreviewCheck();
     void HtmlCheck();
+    /*!
+     \brief
+
+    */
     void CreateNewFile();
+    /*!
+     \brief
+
+    */
     void OpenFile();
+    /*!
+     \brief
+
+    */
     void SaveFile();
+    /*!
+     \brief Сохранить файл как
+
+     \return int
+    */
     int SaveFileAs();
+    /*!
+     \brief Вставить изображение
+
+    */
     void InsertImg();
+    /*!
+     \brief Вставить ссылку
+
+    */
     void InsertLink();
+    /*!
+     \brief Экспорт в PDF
+
+    */
     void ExportToPDF();
+    /*!
+     \brief Экспорт в HTML
+
+    */
     void ExportHtml();
+    /*!
+     \brief Информация о программе
+
+    */
     void AboutProgram();
+    /*!
+     \brief Документация Markdown (англоязычная)
+
+    */
     void MarkdownHelp();
+    /*!
+     \brief Функция проверки на несохранненный данные перед выходом
+
+     \param event Ловим ивент выхода из программы
+    */
     void closeEvent(QCloseEvent *event);
-//popup Windows
-public:
+
+private:
+    /*!
+     \brief Вывод диалогового окно
+
+     \return int
+    */
     int SaveDialog();
 
 
-//Other variables
-public:
-    CodeEditor *MarkdowntextEdit;
-    QTextBrowserFixed* TextPreview;
-    QTextBrowserFixed* htmlPreview;
 
-    QWebEngineView* htmlWeb;
+private:
+    CodeEditor *MarkdowntextEdit; /*! Редактор Markdown*/
+    QTextBrowserFixed* TextPreview; /*!< TODO: describe */
+    QTextBrowserFixed* htmlPreview; /*!< TODO: describe */
+
+    QWebEngineView* htmlWeb; /*!< TODO: describe */
 
 
-    //icons
-    QAction* html_ico;
-    QAction* text_ico;
-    QAction* markdown_ico;
 
-    //StatusBar
-    QLabel* cursorPosition;
-    QLabel* fileSize;
+    QAction* html_ico; /*! Значок в */
+    QAction* text_ico; /*!< TODO: describe */
+    QAction* markdown_ico; /*!< TODO: describe */
 
-    //Файл, с которым происходит работа
-    QFile file;
 
-    bool isLoadFinished = false;
-    bool justCreated = false;
-    bool isChanged = false;
+    QLabel* cursorPosition; /*!< Номер на которой находится курсор */
+    QLabel* fileSize; /*!< Размер файла */
+
+
+    QFile file; /*! Файл с которым происходит работа */
+
+    bool isLoadFinished = false; /*!< TODO: describe */
+    bool justCreated = false; /*! Файл созданный в программме, н */
+    bool isChanged = false; /*!< TODO: describe */
 };
 #endif // MAINWINDOW_H
